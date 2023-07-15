00a424e786bce0464f23ec7dc4b65c4387a200d9
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-15 01:44:48 -0300

    feat: create first user route

---
+++ src/main/java/io/github/carloshendvpm/quarkussocial/rest/UserResource.java
@@ -0,0 +1,26 @@
+package io.github.carloshendvpm.quarkussocial.rest;
+
+import io.github.carloshendvpm.quarkussocial.rest.dto.CreateUserRequest;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+  
+  @POST
+  public Response createUser( CreateUserRequest userRequest ){
+    return Response.ok(userRequest).build();
+  }
+
+  @GET
+  public Response listAllUsers(){
+    return Response.ok().build();
+  }
+}
