ffc9311a0c73d3049a049a10696e67482712e553
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-17 00:03:52 -0300

    refactor: using more specific status for responses

--- src/main/java/io/github/carloshendvpm/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/carloshendvpm/quarkussocial/rest/UserResource.java
@@ -43,9 +43,7 @@ public class UserResource {
     Set<ConstraintViolation<CreateUserRequest>> violations = validator.validate(userRequest);
 
     if(!violations.isEmpty()){ // se o set de violações não estiver vazio
-      ResponseError responseError = ResponseError.createFromValidation(violations);
-      return Response.status(400).entity(responseError).build();
-
+      return ResponseError.createFromValidation(violations).withStatusCode(ResponseError.UNPROCESSABLE_ENTITY_STATUS);
     }
     User user = new User();
     user.setAge(userRequest.getAge());
@@ -53,7 +51,7 @@ public class UserResource {
 
     repository.persist(user);
 
-    return Response.ok(user).build();
+    return Response.status(Response.Status.CREATED.getStatusCode()).entity(user).build();
   }
 
   @GET
@@ -69,7 +67,7 @@ public class UserResource {
     User user = repository.findById(id);
     if(user != null ){
       repository.delete(user);
-      return Response.ok().build();
+      return Response.noContent().build();
     }
     return Response.status(Response.Status.NOT_FOUND).build();
   }
