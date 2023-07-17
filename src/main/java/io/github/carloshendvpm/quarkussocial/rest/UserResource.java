9d4dffa15cd4781006c3d707720965ffd97e22d3
Carlos Henrique <hencarlosdv@gmail.com>
2023-07-16 22:51:29 -0300

    feat: create CRUD using repository instead of panacheEntity

--- src/main/java/io/github/carloshendvpm/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/carloshendvpm/quarkussocial/rest/UserResource.java
@@ -1,12 +1,18 @@
 package io.github.carloshendvpm.quarkussocial.rest;
 
+import io.github.carloshendvpm.quarkussocial.domain.model.User;
+import io.github.carloshendvpm.quarkussocial.domain.repository.UserRepository;
 import io.github.carloshendvpm.quarkussocial.rest.dto.CreateUserRequest;
-import io.github.carloshendvpm.quarkussocial.rest.quarkussocial.domain.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
@@ -16,6 +22,13 @@ import jakarta.ws.rs.core.Response;
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
   
+  private UserRepository repository;
+
+  @Inject
+  public UserResource(UserRepository repository){
+    this.repository = repository;
+  }
+  
   @POST
   @Transactional
   public Response createUser( CreateUserRequest userRequest ){
@@ -23,13 +36,40 @@ public class UserResource {
     user.setAge(userRequest.getAge());
     user.setName(userRequest.getName());
 
-    user.persist();
+    repository.persist(user);
 
     return Response.ok(user).build();
   }
 
   @GET
   public Response listAllUsers(){
-    return Response.ok(User.listAll()).build();
+    PanacheQuery<User> query = repository.findAll();
+    return Response.ok(query.list()).build();
+  }
+
+  @DELETE
+  @Path("{id}")
+  @Transactional // quando é feita alguma alteração no banco o transactional tem que ser utilizado
+  public Response deleteUser(@PathParam("id") Long id){
+    User user = repository.findById(id);
+    if(user != null ){
+      repository.delete(user);
+      return Response.ok().build();
+    }
+    return Response.status(Response.Status.NOT_FOUND).build();
+  }
+
+  @PUT
+  @Path("{id}")
+  @Transactional
+  public Response updateUser(@PathParam("id") Long id, CreateUserRequest userData ){
+    User user = repository.findById(id);
+    if( user != null){
+      user.setName(userData.getName());
+      user.setAge(userData.getAge());
+      //repository.persist(user); isso não é necessário pois com o transactional qualquer alteração que fizer na entidade vai ser commitada quando fizer a transação  
+      return Response.ok(user).build();
+    }
+    return Response.status(Response.Status.NOT_FOUND).build();
   }
 }
